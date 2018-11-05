56684da958c953a02eedb183cd61eb80ad767448
Philip Jonas <philip.jonas@kurtosys.com>
2018-11-05 13:06:01 +0200

    ment-4 || Ui implemented

--- user_auth/src/App.tsx
+++ user_auth/src/App.tsx
@@ -1,9 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
+import { Container, Header, Grid } from "semantic-ui-react";
+import styled from "styled-components";
+
+const ContainerPadding = styled.div`
+  padding: 15px;
+`;
 
 class App extends React.Component {
   public render() {
     return (
-      <h1>HELLO WORLD</h1>
+      <div>
+        <Container>
+          <Grid columns={1} padded={true}>
+            <Grid.Row>
+              <Grid.Column>
+                <Header as="h2">USER LOGIN</Header>
+                <Header as="h2">USER REGISTRATION</Header>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </div>
     );
   }
 }
